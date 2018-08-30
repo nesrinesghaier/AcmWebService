b88de3de8597d7de3c17e5510a2f2f75daba40a5
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2018-08-30 02:01:25 +0100

    third commit

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {UserModule} from "./user/user.module";
+import {AcmerModule} from "./acmer/acmer.module";
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {AcmerService} from "./acmer/acmer.service";
+import {AcmerAddComponent} from "./acmer/acmer-add/acmer-add.component";
+import {AcmerListComponent} from "./acmer/acmer-list/acmer-list.component";
+import {AcmerDetailsComponent} from "./acmer/acmer-details/acmer-details.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule
+    HttpClientModule,
+    AcmerModule,
   ],
-  providers: [],
+  providers: [AcmerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
