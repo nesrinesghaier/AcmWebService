191183f480c8e229d82344f47fb4aad69e723fe3
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2018-10-01 23:56:36 +0100

    branch project

---
+++ backend/src/main/java/com/eniso/acmwebservice/Controller/MainController.java
@@ -0,0 +1,15 @@
+package com.eniso.acmwebservice.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String index(){
+        return "forward:/index.html";
+    }
+}
--- src/main/java/com/eniso/acmwebservice/Entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.eniso.acmwebservice.Entity;
-
-public enum Role {
-    ADMIN,
-    USER
-}
